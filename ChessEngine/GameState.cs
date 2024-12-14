773a4be5998c195be71a5b000e547ed36362211a
KELECHIDAVIS <kboss16058@gmail.com>
2024-12-13 21:36:26 -0800

    Starting On Unity Frontend

---
+++ ChessEngine/GameState.cs
@@ -0,0 +1,9 @@
+﻿public struct GameState
+{
+    Side sideToMove  ; // initially white
+    bool CWK , CWQ  , CBK  , CBQ  ; //castling rights; updated when king or rook moves 
+    ulong EP; // en passant bb; updated on a 2 pawn push 
+    int halfMoveClock ; // how many halfmoves have been done ; at 100 theres a draw
+    int fullMoveNum ;
+    Move nextMove; 
+}
